fa8277b5161498621e1a0a18054385fb8c9da37a
kgellert <gerry9@bk.ru>
2022-07-15 10:31:10 +0300

    fix scrubber layout for convenient use on iphones with safe area fix image glitching (when try to dismiss when animation unfinished)

--- ImageViewer/Source/GalleryViewController.swift
+++ ImageViewer/Source/GalleryViewController.swift
@@ -420,7 +420,7 @@ open class GalleryViewController: UIPageViewController, ItemControllerDelegate {
     fileprivate func layoutScrubber() {
         scrubber.bounds = CGRect(origin: CGPoint.zero, size: CGSize(width: self.view.bounds.width - scrubberRotationInset(), height: 40))
         scrubber.center = self.view.boundsCenter
-        scrubber.frame.origin.y = (footerView?.frame.origin.y ?? self.view.bounds.maxY) - scrubber.bounds.height
+        scrubber.frame.origin.y = (footerView?.frame.origin.y ?? self.view.bounds.maxY - 40) - scrubber.bounds.height
     }
 
     /// Accommodate for scrubber's width on landscape screens by narrowing it
